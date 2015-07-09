f94ba91c3f13dd9d5ef49704a5241cf83f402d6a
Sam Morgan <samorgan@huskers.unl.edu>
2015-07-09 13:40:14 -0500

    trying to fix this project

--- controllers/main.rb
+++ controllers/main.rb
@@ -2,3 +2,14 @@ get "/homepage" do
   erb :"main/homepage"
 end
 
+get "/golfers" do
+  erb :"main/golfers"
+end
+
+get "/outings" do
+  erb :"main/outings"
+end
+
+get "/courses" do
+  erb :"main/courses"
+end
